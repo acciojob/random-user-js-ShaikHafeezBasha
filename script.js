4b4af56b2252835bd6feb18d766513e8a6b7d36c
Shaik Hafeez Basha <115686162+ShaikHafeezBasha@users.noreply.github.com>
2023-08-17 19:42:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
-//your code here
+const res = fetch('https://randomuser.me/api/ ')
+res.then((info) => {
+    let prom = info.json();
+    prom.then((data) => {
+        let img = document.getElementById('img')
+        let h2 = document.getElementById('heading')
+        let para = document.getElementById('para')
+        let ageBtn = document.getElementById('age')
+        let emailBtn = document.getElementById('email')
+        let phoneBtn = document.getElementById('phone')
+        let getUserBtn = document.getElementById('get')
+        ageBtn.addEventListener('click', () => {
+            para.textContent = data.results[0].registered.age;
+        })
+        emailBtn.addEventListener('click', () => {
+            para.textContent = data.results[0].email;
+        })
+        phoneBtn.addEventListener('click', () => {
+            para.textContent = data.results[0].phone;
+        })
+        getUserBtn.addEventListener('click', () => {
+            location.reload();
+        })
+        h2.textContent = data.results[0].name.first + " " + data.results[0].name.last;
+        img.src = data.results[0].picture.large
+        img.alt = 'error'
+    })
+})
+
